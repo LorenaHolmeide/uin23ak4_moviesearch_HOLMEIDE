1376621ed2ec7931534511d3ef874aa1db65147e
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-12 22:19:20 +0100

    Added Component branch with components

---
+++ src/components/MoviePage.js
@@ -0,0 +1,27 @@
+import { useParams } from "react-router-dom"
+import { useState, useEffect } from "react";
+//export default function MoviePage({ movies })
+export default function MoviePage() {
+    const { slug } = useParams()
+    //const movie = movies.find((m) => m.Title.replace(/\s/g, "-").toLowerCase() === slug)
+
+    const [movie, setMovie] = useState({});
+    useEffect(() => {
+        fetch(`http://www.omdbapi.com/?apikey=2b4982d&t=${slug}`)
+            .then(response => response.json())
+            .then(data => setMovie(data))
+            .catch(error => console.log(error));
+    }, [slug]);
+
+    return (
+        <section>
+            <img src={movie.Poster} alt={movie.Title} />
+            <h1>{movie.Title}</h1>
+            <p>Year released: {movie.Released}</p>
+            <p>Genre: {movie.Genre}</p>
+            <p>Director: {movie.Director}</p>
+            <p>Actors: {movie.Actors}</p>
+            <p>Awards: {movie.Awards}</p>
+        </section>
+    )
+}
--- src/components/Moviecard.js
+++ src/components/Moviecard.js
@@ -1,3 +1,10 @@
-export default function Moviecard() {
-    return
+import { Link } from "react-router-dom";
+export default function MovieCard({ title, img, slug }) {
+    return (
+        <article className="movie-card">
+            <img src={img} alt={title} />
+            <h3>{title}</h3>
+            <Link to={slug} className="link"></Link>
+        </article>
+    )
 }
