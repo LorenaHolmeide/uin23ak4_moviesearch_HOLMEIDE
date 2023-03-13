47ccecbcb70c1c141d539f473fb59c70709b4697
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-13 16:33:02 +0100

    added button to Card and scss Page

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,5 +1,15 @@
 //ComponentBranch
-import { Link } from "react-router-dom";
+export default function MovieCard({ title, img, slug }) {
+    return (
+        <article className="movie-card">
+            <img src={img} alt={title} />
+            <h3>{title}</h3>
+            <button onClick={() => { window.location.href = slug }} className="link">Mer info</button>
+        </article>
+    )
+}
+
+/*import { Link } from "react-router-dom";
 export default function MovieCard({ title, img, slug }) {
     return (
         <article className="movie-card">
@@ -8,4 +18,5 @@ export default function MovieCard({ title, img, slug }) {
             <Link to={slug} className="link"></Link>
         </article>
     )
-}
+}*/
+
