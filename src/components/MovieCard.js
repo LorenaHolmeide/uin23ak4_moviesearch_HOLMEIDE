9b77d1d4680cfd4db4f6c2deeb53fd6547d99cbb
LorenaHolmeide <89775879+LorenaHolmeide@users.noreply.github.com>
2023-03-15 17:18:39 +0100

    updated components

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,22 +1,31 @@
 //ComponentBranch
+
+import { useState, useEffect } from "react";
+
 export default function MovieCard({ title, img, slug }) {
+
+    const [movie, setMovie] = useState({});
+
+    useEffect(() => {
+        fetch(`http://www.omdbapi.com/?apikey=2b4982d&t=${title}`)
+            .then(response => response.json())
+            .then(data => setMovie(data))
+            .catch(error => console.log(error));
+    }, [title]);
+
     return (
         <article className="movie-card">
             <img src={img} alt={title} />
             <h3>{title}</h3>
+            <p>Released: {movie.Released}</p>
+            <p>Genre: {movie.Genre}</p>
+            <p>Actors: {movie.Actors}</p>
+            <p>Director: {movie.Director}</p>
+            <p>Awards: {movie.Awards}</p>
             <button onClick={() => { window.location.href = slug }} className="link">Mer info</button>
         </article>
     )
 }
 
-/*import { Link } from "react-router-dom";
-export default function MovieCard({ title, img, slug }) {
-    return (
-        <article className="movie-card">
-            <img src={img} alt={title} />
-            <h3>{title}</h3>
-            <Link to={slug} className="link"></Link>
-        </article>
-    )
-}*/
+
 
--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -15,13 +15,37 @@ export default function MoviePage() {
 
     return (
         <section className="movie-page" >
-            <img src={movie.Poster} alt={movie.Title} />
-            <h1>{movie.Title}</h1>
-            <p>Year released: {movie.Released}</p>
-            <p>Genre: {movie.Genre}</p>
-            <p>Director: {movie.Director}</p>
-            <p>Actors: {movie.Actors}</p>
-            <p>Awards: {movie.Awards}</p>
+            <div className="movie-info">
+                <img src={movie.Poster} alt={movie.Title} />
+                <h1>{movie.Title}</h1>
+                <em>Plot: {movie.Plot}</em>
+                <p>Runtime: {movie.Runtime}</p>
+                <p>Language: {movie.Language}</p>
+                <p>Type: {movie.Type}</p>
+                <p>Production: {movie.Production}</p>
+                <p>imdbRating: {movie.imdbRating}</p>
+                <p>imdbVotes: {movie.imdbVotes}</p>
+                <p>imdbID: {movie.imdbID}</p>
+                <ul> <li>Ratings:</li>
+                    {movie.Ratings &&
+                        movie.Ratings.map((rating) => (
+                            <li key={rating.Source}>
+                                <span>{rating.Source}: </span>
+                                <span>{rating.Value}</span>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+            <div className="movie-details">
+                <p>BoxOffice: {movie.BoxOffice}</p>
+                <p>Website: {movie.Website}</p>
+                <p>Year released: {movie.Released}</p>
+                <p>Genre: {movie.Genre}</p>
+                <p>Director: {movie.Director}</p>
+                <p>Actors: {movie.Actors}</p>
+                <p>Awards: {movie.Awards}</p>
+                <p>Respone:{movie.Response}</p>
+            </div>
         </section>
     )
 }
